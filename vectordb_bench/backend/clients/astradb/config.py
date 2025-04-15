2eb680b58d93d4882ff0126cf8da03e4e84a3a4d
Madhavan Sridharan <madhavan.sridharan@datastax.com>
2025-04-15 13:23:11 -0400

    Make environment variable name consistent

--- vectordb_bench/backend/clients/astradb/config.py
+++ vectordb_bench/backend/clients/astradb/config.py
@@ -7,12 +7,12 @@ class AstraDBConfig(DBConfig):
     AstraDB configuration class.
 
     Attributes:
-        astra_token (str): The Astra authentication token. This can also be set using the ASTRA_API_KEY environment variable.
+        astra_token (str): The Astra authentication token. This can also be set using the ASTRA_DB_APPLICATION_TOKEN environment variable.
         scb_path (str): The path to the secure connect bundle. This can also be set using the ASTRA_SCB_PATH environment variable.
         keyspace (Optional[str]): The keyspace to use in the database. This program will create it if it doesn't exist. Defaults to 'default_keyspace'.
     """
     # This starts with AstraCS:...
-    astra_token: SecretStr | None = os.environ.get("ASTRA_API_KEY", None)
+    astra_token: SecretStr | None = os.environ.get("ASTRA_DB_APPLICATION_TOKEN", None)
     # This is the path to the secure connect bundle
     scb_path: str | None = os.environ.get("ASTRA_SCB_PATH", None)
     keyspace: str | None = "default_keyspace"
